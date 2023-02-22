98db477ec18579d1b171bcbf6248afce568189ee
lmoreau21 <61840342+lmoreau21@users.noreply.github.com>
2023-02-22 13:17:27 -0600

    changed to buttons

--- sound.js
+++ sound.js
@@ -1,58 +1,56 @@
-let sounds;
+
 let shifter = new Tone.PitchShift(2).toMaster();
-let slider;
-let checkbox;
+
+let sounds = new Tone.Players({Announcement:"mixkit-clear-announce-tones-2861.wav",Death:"mixkit-arcade-retro-game-over-213.wav",Race:"mixkit-race-countdown-1953.wav",Bleep:"mixkit-winning-an-extra-bonus-2060.wav"}).connect(shifter);
+const delay = new Tone.FeedbackDelay("8n", 0);
+let name = ["Announcement", "Death", "Race", "Bleep"]
+let buttons = [];
+let slider, delaySlider;
 
 //Tone.Transport.start();
+
 function setup(){
   createCanvas(windowWidth-20,windowHeight-20);
-  
-  background(0);
-  slider = createSlider(-10, 10,0,1)
-  size = height/3;
-  sounds = new Tone.Players({soundOne:"mixkit-clear-announce-tones-2861.wav",soundTwo:"mixkit-arcade-retro-game-over-213.wav",soundThree:"mixkit-race-countdown-1953.wav",soundFour:"mixkit-winning-an-extra-bonus-2060.wav"}).connect(shifter);
-}
-function draw(){
-  textAlign('Center')
-  
-  resizeCanvas(windowWidth-20, windowHeight-20);
   background('lightblue');
+  sounds.connect(delay);
+  delay.toDestination();
+
+  slider = createSlider(-10, 10,0,1)
+  
+  delaySlider = createSlider(0, 1,0,.1);
+  
+  textAlign(CENTER)
+  fill('black');
+  resizeCanvas(windowWidth-20, windowHeight-20);
+  
   slider.position(width/8,50)
   slider.style('width',width*3/4+'px')
 
-  fill('red')
-  shapeOne =  circle(width*1/4,height/4+30,size)
-  fill('green')
-  shapeTwo = circle(width*3/4,height/4+30,size)
-  fill('blue')
-  shapeThree = circle(width*1/4,height*3/4,size)
-  fill('orange')
-  shapeFour = circle(width*3/4,height*3/4,size)
-  
-  noStroke();
-  textSize(24);
+  delaySlider.position(width/8,150)
+  delaySlider.style('width',width*3/4+'px')
 
+  name.forEach((word, index) => {
+    buttons[index] = createButton(word);
+    buttons[index].position(width/2-width/6, index*height/6+180);
+    buttons[index].size(width/3,100);
+    buttons[index].mousePressed( () => buttonSound(word))
+  })
+
+  shifter.wet.value = 1;
+}
+function draw(){
+  background('lightblue');
+  
+  textSize(24);
+  shifter.pitch = slider.value();
+  delay.feedback.value = delaySlider.value();
 
   fill("black")
-  text("Pitch: "+slider.value(),width/2-40,30)
-  stroke('black')
-  fill('white')
-  text("Announcement",width*1/4-textWidth("Announcement")/2,height/4+36)
-  text("Death Sound",width*3/4-textWidth("Death Sound")/2,height/4+36)
-  text("Race Countdown",width*1/4-textWidth("Race Countdown")/2,height*3/4+6)
-  text("Bleep Sound",width*3/4-textWidth("Bleep Sound")/2,height*3/4+6)
-  shifter.wet.value = 1;
-  shifter.pitch = slider.value();
+  text("Pitch: "+slider.value(),width/2-15,30);
+  text("Delay: "+delaySlider.value(),width/2-15,130) 
 }
 
-function mouseClicked() {
-  if (dist(mouseX, mouseY, width*1/4,height/4+30) <= size/2) {
-    sounds.player("soundOne").start();
-  } else if (dist(mouseX, mouseY, width*3/4,height/4+30) <= size/2) {
-    sounds.player("soundTwo").start();
-  } else if (dist(mouseX, mouseY, width*1/4,height*3/4) <= size/2) {
-    sounds.player("soundThree").start();
-  }else if (dist(mouseX, mouseY, width*3/4,height*3/4) <= size/2) {
-    sounds.player("soundFour").start();
-  }
+function buttonSound(theSound) {
+  sounds.player(theSound).start();
 }
+
